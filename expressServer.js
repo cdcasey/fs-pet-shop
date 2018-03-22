eab5500d0069b80dfe1660cda99a1a4d43f8aee4
Chris Casey <cdcasey@gmail.com>
2018-03-22 14:00:34 -0500

    POST works

--- expressServer.js
+++ expressServer.js
@@ -13,7 +13,7 @@ app.route('/pets')
         fs.readFile('./pets.json', 'utf8', (err, data) => {
             if (err) {
                 console.error(err.stack);
-                return res.sendStatus(500);
+                return response.sendStatus(500);
             }
             let pets = JSON.parse(data);
 
@@ -43,9 +43,40 @@ app.route('/pets/:id')
         })
     })
 
-app.use(function (require, response) {
-    response.statusCode = 404;
-    response.send('Not Found');
+app.route('/pets')
+    .post((request, response) => {
+        let body = '';
+        request.on('data', (chunk) => {
+            body += chunk;
+        }).on('end', () => {
+            let message;
+            const newPet = JSON.parse(body);
+            if (!newPet.name || !newPet.age || !newPet.kind) {
+                message = "New pet must have name, age, & kind attributes.";
+                response.statusCode = 400;
+                response.set('Content-Type', 'text/plain');
+                response.send(message);
+            } else {
+                fs.readFile('./pets.json', (err, data) => {
+                    let pets = JSON.parse(data);
+                    pets.push(newPet);
+                    let myData = JSON.stringify(pets);
+                    fs.writeFile('./pets.json', myData, (err) => {
+                        if (err) {
+                            throw err;
+                        } else {
+                            response.set('Content-Type', 'application/json');
+                            message = JSON.stringify(newPet);
+                            response.send(message);
+                        }
+                    })
+                })
+            }
+        })
+    })
+
+app.use(function (request, response) {
+    response.status(404).send('Not Found');
 });
 
 app.listen(port, function () {
